db48a3a12abfa409fb8761c0623aa484df237457
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-04-27 15:52:36 +0900

    affine

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <Novice.h>
 #include <math.h>
-#define PI 3.14
+#define PI 3.141592653589
 
 const char kWindowTitle[] = "LE2B_02_アスマ_ショウタ";
 
@@ -14,7 +14,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	float SuqareWidht = 128.0;	//横幅
+	float SuqareHeight = 128.0;	//立幅
+	//反時計回りの座標
 	float box[4][3] = {
+	  {128.0f, 128.0f, 1.0f},
+	  {128.0f, 256.0f, 1.0f},
+	  {256.0f, 256.0f, 1.0f},
+	  {256.0f,  128.0f, 1.0f},
+	};
+	float box2[4][3] = {
+	  {128.0f, 128.0f, 1.0f},
+	  {128.0f, 256.0f, 1.0f},
+	  {256.0f, 256.0f, 1.0f},
+	  {256.0f, 128.0f, 1.0f},
+	};
+	float box3[4][3] = {
+	  {128.0f, 128.0f, 1.0f},
+	  {128.0f, 256.0f, 1.0f},
+	  {256.0f, 256.0f, 1.0f},
+	  {256.0f, 128.0f, 1.0f},
+	};
+	float box4[4][3] = {
 	  {128.0f, 128.0f, 1.0f},
 	  {128.0f, 256.0f, 1.0f},
 	  {256.0f, 256.0f, 1.0f},
@@ -24,22 +45,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//平行移動の行列
 	float move[3][3] = {
 	  {1.0f, 0.0f, 100.0f},
-      {0.0f, 1.0f, 100.0f},
+      {0.0f,1.0f, 100.0f},
       {0.0f, 0.0f, 1.0f}
     };
 
 	//拡大縮小の行列
 	float scale[3][3] = {
 	  {2.0f, 0.0f, 0},
-	  {0.0f, 1.0f, 0},
+	  {0.0f, 2.0f, 0},
 	  {0.0f, 0.0f, 1},
 	};
 
 	//回転の行列
-	float role[3][3] = {
-	  {0.0f, -0.0f, 0},
-	  {0.0f, 0.0f, 0},
-	  {0.0f, 0.0f, 1},
+	float roll[3][3] = {
+	  {cos(PI / 3), -sin(PI / 3), 0},
+	  {sin(PI / 3), cos(PI / 3), 0},
+	  {0.0f,0.0f, 1},
 	};
 
 	//平行移動
@@ -49,19 +70,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		box[i][2] = move[2][0] * box[i][0] + move[2][1] * box[i][1] + move[2][2] * box[i][2];
 	}
 
-	////拡大縮小
-	//for (int i = 0; i < 4; i++) {
-	//	box[i][0] = scale[0][0] * box[i][0] + scale[0][1] * box[i][1] + scale[0][2] * box[i][2];
-	//	box[i][1] = scale[1][0] * box[i][0] + scale[1][1] * box[i][1] + scale[1][2] * box[i][2];
-	//	box[i][2] = scale[2][0] * box[i][0] + scale[2][1] * box[i][1] + scale[2][2] * box[i][2];
-	//}
+	//保存用の変数
+	float squareScaleX = 0.0f;
+	float squareScaleY = 0.0f; 
 
-	////回転
-	// for (int i = 0; i < 4; i++) {
-	//	box[i][0] = role[0][0] * box[i][0] + role[0][1] * box[i][1] + role[0][2] * box[i][2];
-	//	box[i][1] = role[1][0] * box[i][0] + role[1][1] * box[i][1] + role[1][2] * box[i][2];
-	//	box[i][2] = role[2][0] * box[i][0] + role[2][1] * box[i][1] + role[2][2] * box[i][2];
-	// }
+	//拡大縮小	左上原点
+	for (int i = 0; i < 4; i++) {
+		squareScaleX = box2[i][0] - SuqareWidht;
+		squareScaleY = box2[i][1] - SuqareWidht;
+
+		box2[i][0] =
+		  scale[0][0] * squareScaleX + scale[0][1] * squareScaleY + scale[0][2] * box2[i][2];
+		box2[i][1] =
+		  scale[1][0] * squareScaleX + scale[1][1] * squareScaleY + scale[1][2] * box2[i][2];
+		box2[i][2] =
+		  scale[2][0] * squareScaleX + scale[2][1] * squareScaleY + scale[2][2] * box2[i][2];
+
+		box2[i][0] += SuqareWidht;
+		box2[i][1] += SuqareHeight;
+	}
+
+	float squareRollX = 0.0f;
+	float squareRollY = 0.0f;
+	//回転	左上原点
+	for (int i = 0; i < 4; i++) {
+		squareRollX = box3[i][0] - SuqareWidht;
+		squareRollY = box3[i][1] - SuqareHeight;
+
+		box3[i][0] = squareRollX * roll[0][0] + squareRollY * roll[0][1];
+		box3[i][1] = squareRollX * roll[1][0] + squareRollY * roll[1][1];
+
+		box3[i][0] += SuqareWidht;
+		box3[i][1] += SuqareHeight;
+	}
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -81,13 +122,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↑更新処理ここまで
 		///
 
+		//平行移動の行列
+
 		///
 		/// ↓描画処理ここから
 		///
 		
+		//平行移動
 		for (int i = 0; i < 4; i++) {
 			Novice::DrawLine(
 			  box[i][0], box[i][1], box[(i + 1) % 4][0], box[(i + 1) % 4][1], 0x86BD3EFF);
+			Novice::DrawLine(
+			  box2[i][0], box2[i][1], box2[(i + 1) % 4][0], box2[(i + 1) % 4][1], 0x86BD3EFF);
+			Novice::DrawLine(
+			  box3[i][0], box3[i][1], box3[(i + 1) % 4][0], box3[(i + 1) % 4][1], 0x86BD3EFF);
+			Novice::DrawLine(
+			  box4[i][0], box4[i][1], box4[(i + 1) % 4][0], box4[(i + 1) % 4][1], 0x86BD3EFF);
 		}
 
 		///
