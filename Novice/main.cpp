80cce91b9eca72f284485ab487eac1ea61a840a2
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-04-27 11:38:02 +0900

    move

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,4 +1,6 @@
 ﻿#include <Novice.h>
+#include <math.h>
+#define PI 3.14
 
 const char kWindowTitle[] = "LE2B_02_アスマ_ショウタ";
 
@@ -12,20 +14,55 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	float circle[3] = {100.0f, 100.0f, 1};
-	float circleR = 32.0f;
+	float box[4][3] = {
+	  {128.0f, 128.0f, 1.0f},
+	  {128.0f, 256.0f, 1.0f},
+	  {256.0f, 256.0f, 1.0f},
+	  {256.0f, 128.0f, 1.0f},
+	};
 
+	//平行移動の行列
 	float move[3][3] = {
 	  {1.0f, 0.0f, 100.0f},
       {0.0f, 1.0f, 100.0f},
       {0.0f, 0.0f, 1.0f}
     };
 
+	//拡大縮小の行列
+	float scale[3][3] = {
+	  {2.0f, 0.0f, 0},
+	  {0.0f, 1.0f, 0},
+	  {0.0f, 0.0f, 1},
+	};
+
+	//回転の行列
+	float role[3][3] = {
+	  {0.0f, -0.0f, 0},
+	  {0.0f, 0.0f, 0},
+	  {0.0f, 0.0f, 1},
+	};
+
+	//平行移動
 	for (int i = 0;i < 4;i++) {
-		circle[i] = move[0][0] * 
+		box[i][0] = move[0][0] * box[i][0] + move[0][1] * box[i][1] + move[0][2] * box[i][2];
+		box[i][1] = move[1][0] * box[i][0] + move[1][1] * box[i][1] + move[1][2] * box[i][2];
+		box[i][2] = move[2][0] * box[i][0] + move[2][1] * box[i][1] + move[2][2] * box[i][2];
 	}
-	
-	int speed = 10;
+
+	////拡大縮小
+	//for (int i = 0; i < 4; i++) {
+	//	box[i][0] = scale[0][0] * box[i][0] + scale[0][1] * box[i][1] + scale[0][2] * box[i][2];
+	//	box[i][1] = scale[1][0] * box[i][0] + scale[1][1] * box[i][1] + scale[1][2] * box[i][2];
+	//	box[i][2] = scale[2][0] * box[i][0] + scale[2][1] * box[i][1] + scale[2][2] * box[i][2];
+	//}
+
+	////回転
+	// for (int i = 0; i < 4; i++) {
+	//	box[i][0] = role[0][0] * box[i][0] + role[0][1] * box[i][1] + role[0][2] * box[i][2];
+	//	box[i][1] = role[1][0] * box[i][0] + role[1][1] * box[i][1] + role[1][2] * box[i][2];
+	//	box[i][2] = role[2][0] * box[i][0] + role[2][1] * box[i][1] + role[2][2] * box[i][2];
+	// }
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -39,8 +76,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		
-		
 
 		///
 		/// ↑更新処理ここまで
@@ -50,7 +85,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-		Novice::DrawEllipse(circle[0], circle[1], circleR, circleR, 0.0f, WHITE, kFillModeSolid);
+		for (int i = 0; i < 4; i++) {
+			Novice::DrawLine(
+			  box[i][0], box[i][1], box[(i + 1) % 4][0], box[(i + 1) % 4][1], 0x86BD3EFF);
+		}
 
 		///
 		/// ↑描画処理ここまで
